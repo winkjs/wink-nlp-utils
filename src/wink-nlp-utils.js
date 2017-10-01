789833fd2bccd02bc5164b9d6e6369ef8fc96f38
sanjayaksaxena <sanjayaksaxena@yahoo.com>
2017-10-01 16:48:01 +0530

    feat(wink-nlp-util): add string.soundex() function

--- src/wink-nlp-utils.js
+++ src/wink-nlp-utils.js
@@ -561,6 +561,65 @@ prepare.string.phonetize = function ( s ) {
       return ( p );
 }; // phonetize()
 
+// Soundex Code for alphabets.
+/* eslint-disable object-property-newline */
+var soundexMap = {
+  A: 0, E: 0, I: 0, O: 0, U: 0, Y: 0,
+  B: 1, F: 1, P: 1, V: 1,
+  C: 2, G: 2, J: 2, K: 2, Q: 2, S: 2, X: 2, Z: 2,
+  D: 3, T: 3,
+  L: 4,
+  M: 5, N: 5,
+  R: 6
+};
+
+// #### soundex
+
+// Produces the soundex code from the input `word`. Default value of maxLength
+// is **4**.
+prepare.string.soundex = function ( word, maxLength ) {
+  // Upper case right in the begining.
+  var s = ( word.length ) ? word.toUpperCase() : '?';
+  var i,
+      imax = s.length;
+  // Soundex code builds here.
+  var sound = [];
+  // Helpers - `ch` is a char from `s` and `code/prevCode` are sondex codes
+  // for consonants.
+  var ch, code,
+      prevCode = 9;
+  // Use default of 4.
+  var maxLen = maxLength || 4;
+  // Iterate through every character.
+  for ( i = 0; i < imax; i += 1 ) {
+    ch = s[ i ];
+    code = soundexMap[ ch ];
+    if ( i ) {
+      // Means i is > 0.
+      // `code` is either (a) `undefined` if an unknown character is
+      // encountered including `h & w`, or (b) `0` if it is vowel, or
+      // (c) the soundex code for a consonant.
+      if ( code && code !== prevCode ) {
+        // Consonant and not adjecant duplicates!
+        sound.push( code );
+      } else if ( code !== 0 ) {
+        // Means `h or w` or an unknown character: ensure `prevCode` is
+        // remembered so that adjecant duplicates can be handled!
+        code = prevCode;
+      }
+    } else {
+      // Retain the first letter
+      sound.push( ch );
+    }
+    prevCode = code;
+  }
+  s = sound.join( '' );
+  // Always ensure minimum length of 4 characters for maxLength > 4.
+  if ( s.length < 4 ) s += '000';
+  // Return the required length.
+  return s.substr( 0, maxLen );
+}; // soundex()
+
 // ### Prepare.Tokens Name Space
 
 // Create prepare.tokens name space.

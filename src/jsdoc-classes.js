d212c7cfeeac0d7885368bfd734070cafcf903f5
Prateek Saxena <prtksxna@gmail.com>
2018-10-25 17:08:38 +0530

    docs: change typedefs to classes

---
+++ src/jsdoc-classes.js
@@ -0,0 +1,42 @@
+/**
+ * @classdesc Indexer object
+ * @class Indexer
+ * @hideconstructor
+ */
+
+/**
+ * Incrementally builds index for each `element/itsIndex` combination passed.
+ *
+ * @method Indexer#build
+ * @param elements
+ * @param itsIndex
+ */
+
+ /**
+  * Used to access the index. This index is in a form of an object that contains
+  * each element as key. The value of each key is an array
+  * containing all index positions to the element in question. Note these index positions
+  * are nothing but each `itsIndex` value passed for the `element`.
+  *
+  * @method Indexer#result
+  * @returns {Object}
+  */
+
+/**
+ * @classdesc WordsFilter
+ * @class WordsFilter
+ * @hideconstructor
+ */
+
+ /**
+  * Contains the set created from the array `words`.
+  *
+  * @method WordsFilter#set
+  */
+
+  /**
+   * Used with the array's filter method to exclude the `words` or mapped
+   * `words` if `givenMappers` are defined.
+   *
+   * @method WordsFilter#exclude
+   */
